6baf8c5ff25e05a03c18ccc8609383fe65fd6567
Carlos Abreu <carlosrsabreu@gmail.com>
2021-12-24 18:58:47 +0000

    feat(global): initial commit

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Carlos Silva Abreu',
+    site_url: 'https://carlosrsabr.eu',
+    feed_url: 'https://carlosrsabr.eu/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
